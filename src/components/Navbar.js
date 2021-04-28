b9c261b3a02952d976956490f32174ffa1ebcf29
Anna <annamariej1015@gmail.com>
2021-04-27 17:07:36 -0700

    feature: added Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,8 +2,20 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return(
-        <div>Title</div>
-    )
+        <nav class="navbar navbar-expand-lg navbar-light bg-warning navbar-style">
+        <a class="navbar-brand" href="#">Anna's Agenda</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+          <ul class="navbar-nav">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    );
 };
 
 export default Navbar;
